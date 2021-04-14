8688fa7006408d47ebe421db6c3f782ea55c9509
Craig <craigwilsonmusic@gmail.com>
2021-04-14 14:58:17 +0100

    MVP complete

--- Models/order.py
+++ Models/order.py
@@ -1,2 +1,12 @@
-class Order:
-    pass
+class Order():
+    
+    def __init__ (self, customer_name, order_date, quantity, food_name):
+        self.customer_name = customer_name
+        self.order_date = order_date
+        self.quantity = quantity
+        self.food_name = food_name
+        self.items_ordered = []
+    
+
+    def add_food_to_items_ordered(self, item):
+        self.items_ordered.append(item)
---
+++ Models/order_list.py
@@ -0,0 +1,6 @@
+from models.order import *
+
+order1 = Order("Mark", "14/04/2021", 1, "Pie")
+order2 = Order("Craig", "14/04/2021", 1, "Sausage Roll")
+
+orders = [order1, order2]
